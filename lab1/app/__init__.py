70e3c81e5ea9ef53cc1a65555e292df91321f009
leavemeal0ne <vlad.makaruk246@gmail.com>
2022-12-30 21:12:23 +0200

    [ADD] Swagger

--- lab1/app/__init__.py
+++ lab1/app/__init__.py
@@ -1,5 +1,13 @@
-from flask import Flask, jsonify , abort, make_response, request
+from flask import Flask
 
 app = Flask(__name__)
+
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config['API_TITLE'] = "Finance REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 from app import view
--- lab1/app/view.py
+++ lab1/app/view.py
@@ -4,11 +4,6 @@ from app.resources.users import blp as UserBluePrint
 from app.resources.categories import blp as CategoriesBluePrint
 from app.resources.records import blp as RecordsBluePrint
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config['API_TITLE'] = "Finance REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
 
 api = Api(app)
 api.register_blueprint(UserBluePrint)
